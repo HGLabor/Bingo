75c0cb04fc4964d9d7a3d7e8478c307f78ae08e4
copyandexecute <58366920+copyandexecute@users.noreply.github.com>
2021-09-02 19:50:43 +0200

    maprenderer cleanup

--- src/main/kotlin/de/hglabor/rendering/MapManager.kt
+++ src/main/kotlin/de/hglabor/rendering/MapManager.kt
@@ -54,78 +54,29 @@ object LaborMapRenderer : MapRenderer() {
 
     }
 
-    val renderCount = hashMapOf<Player, Int>()
-
     override fun render(map: MapView, canvas: MapCanvas, player: Player) {
-        /*
-        if(!renderCount.containsKey(player)) {
-            renderCount[player] = 1
-            renderMap(canvas, player)
-        } else {
-            renderCount[player] = renderCount[player]!!.plus(1)
-            if(renderCount[player]!! < Settings.itemCount) {
-                renderMap(canvas, player)
-            }
-        }
-         */
-        if (player.inventory.itemInOffHand.hasItemMeta()) {
-            if (player.inventory.itemInOffHand.itemMeta!!.hasDisplayName()) {
-                if (player.inventory.itemInOffHand.itemMeta!!.displayName.contains("Bingo")) {
-                    /*
-                    for (material in GameManager.materials) {
-                        var texture = "/textures/"
-                        val isNether = LootSet.NETHER.materials.keys.contains(material)
-                        if(isNether) {
-                            texture+="nether/"
-                        }
-                        var customPath = if(isNether) {
-                            LootSet.NETHER.materials[material]
-                        } else {
-                            LootSet.OVERWORLD.materials[material]
-                        }
-                        if(customPath?.isEmpty() == true) {
-                            customPath = material.name.toLowerCase()
-                        }
-                        val bufferedImage = ImageIO.read(javaClass.getResourceAsStream("$texture$customPath.png"))
-                        cachedImages[material] = bufferedImage
+        val itemInOffHand = player.inventory.itemInOffHand
+        if (!itemInOffHand.hasItemMeta()) return
+        if (itemInOffHand.itemMeta!!.hasDisplayName()) return
+        if (!itemInOffHand.itemMeta!!.displayName.contains("Bingo")) return
+        canvas.drawText(35, 4, MinecraftFont.Font, "§20;HGLABOR.DE")
+        var x = 8
+        var y = 16
+        player.user.bingoField?.forEach { col ->
+            col.forEach { rowItem ->
+                kotlin.runCatching {
+                    if (player.hasChecked(rowItem)) {
+                        canvas.drawImage(x, y, cross)
+                    } else {
+                        cachedImages[rowItem]?.let { canvas.drawImage(x, y, it) }
                     }
-                     */
-                    canvas.drawText(35, 4, MinecraftFont.Font, "§20;HGLABOR.DE")
-                    var x = 8
-                    var y = 16
-                    var drawn = 0
-                    player.user.bingoField?.forEach { col ->
-                        col.forEach { rowItem ->
-                            kotlin.runCatching {
-                                if (player.hasChecked(rowItem)) {
-                                    canvas.drawImage(x, y, cross)
-                                } else {
-                                    cachedImages[rowItem]?.let { canvas.drawImage(x, y, it) }
-                                }
-                            }.onFailure {
-                                broadcast("${KColors.TOMATO}${rowItem.name}")
-                            }
-                            x += 16
-                        }
-                        x = 8
-                        y += 16
-                    }
-                    /* for (material in GamePhaseManager.materials) {
-                         kotlin.runCatching {
-                             cachedImages[material]?.let { canvas.drawImage(x,y, it) }
-                         }.onFailure {
-                             broadcast("${KColors.TOMATO}${material.name}")
-                         }
-                         x+=16
-                         drawn+=1
-                         //TODO if(drawn % 7 == 0 && drawn in 7..49)
-                         if(drawn == 7 || drawn == 14 || drawn == 21 || drawn == 28 || drawn == 35 || drawn == 42 || drawn == 49) {
-                             x=8
-                             y+=16
-                         }
-                     } */
+                }.onFailure {
+                    broadcast("${KColors.TOMATO}${rowItem.name}")
                 }
+                x += 16
             }
+            x = 8
+            y += 16
         }
     }
 }
