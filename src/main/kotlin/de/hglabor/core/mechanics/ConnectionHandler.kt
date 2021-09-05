a2ff232dbc6f456fd9eae65c255e1f2dde3510a8
copyandexecute <58366920+copyandexecute@users.noreply.github.com>
2021-09-05 21:30:11 +0200

    Fixed rejoining

--- src/main/kotlin/de/hglabor/core/mechanics/ConnectionHandler.kt
+++ src/main/kotlin/de/hglabor/core/mechanics/ConnectionHandler.kt
@@ -8,6 +8,7 @@ import org.bukkit.event.player.PlayerLoginEvent
 
 object ConnectionHandler {
     fun handlePlayerLoginEvent(event: PlayerLoginEvent) {
+        if (event.player.canLogin) return
         if(!event.player.canLogin) {
             event.disallow(PlayerLoginEvent.Result.KICK_OTHER, Localization.getUnprefixedMessage("bingo.died", event.player.locale))
         } else if(GamePhaseManager.phase is InGamePhase) {
